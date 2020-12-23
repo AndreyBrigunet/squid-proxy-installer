07448bce88b7bd8bbed9da713807bc24c324645a
Andrei Brigunet <andreybrigunet@gamil.com>
2020-12-23 17:35:28 +0200

    small bugs

--- squid3-install.sh
+++ squid3-install.sh
@@ -20,9 +20,9 @@ if cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 16.04"; then
     /usr/bin/apt update
     /usr/bin/apt -y install apache2-utils squid3
     touch /etc/squid/passwd
-    /bin/rm -f squid_conf
+    /bin/rm -f $squid_conf
     /usr/bin/touch /etc/squid/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O squid_conf https://raw.githubusercontent.com/andreybrigunet/squid-proxy-installer/master/squid.conf
+    /usr/bin/wget --no-check-certificate -O $squid_conf https://raw.githubusercontent.com/andreybrigunet/squid-proxy-installer/master/squid.conf
     
     # Cauta REPLACE_SQUID_PORT si schimba $PORT
     sed -i "s/$search/$PORT/" $squid_conf
