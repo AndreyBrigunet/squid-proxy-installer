b6e18d4a5772da2ec1b1d6aae037492c421313fb
Andrei Brigunet <andreybrigunet@gamil.com>
2020-12-23 17:05:27 +0200

    My update

--- README.md
+++ README.md
@@ -2,16 +2,14 @@
 
 Auto install Squid 3 proxy on
 
-Ubuntu 14.04
 Ubuntu 16.04
-Debian 8 
 
 ## Install Squid
 
 To install, run the script
 
 ```
-wget https://raw.githubusercontent.com/HostOnNet/squid-proxy-installer/master/squid3-install.sh
+wget https://raw.githubusercontent.com/andreybrigunet/squid-proxy-installer/master/squid3-install.sh
 chmod 755 squid3-install.sh
 sudo ./squid3-install.sh
 ```
--- squid3-install.sh
+++ squid3-install.sh
@@ -5,48 +5,40 @@
 # Email: admin@hostonnet.com
 # Github: https://github.com/HostOnNet/squid-proxy-installer
 
+USERNAME = "squser"
+PASSWORD = "user#123456"
+PORT = "user#123456"
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+ip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+
 if cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 16.04"; then
     /usr/bin/apt update
     /usr/bin/apt -y install apache2-utils squid3
     touch /etc/squid/passwd
     /bin/rm -f /etc/squid/squid.conf
     /usr/bin/touch /etc/squid/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/hostonnet/squid-proxy-installer/master/squid.conf
-    /sbin/iptables -I INPUT -p tcp --dport 49951 -j ACCEPT
+    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/andreybrigunet/squid-proxy-installer/master/squid.conf
+    /sbin/iptables -I INPUT -p tcp --dport $PORT -j ACCEPT
     /sbin/iptables-save
     service squid restart
     update-rc.d squid defaults
-elif cat /etc/*release | grep DISTRIB_DESCRIPTION | grep "Ubuntu 14.04"; then
-    /usr/bin/apt update
-    /usr/bin/apt -y install apache2-utils squid3
-    touch /etc/squid3/passwd
-    /bin/rm -f /etc/squid3/squid.conf
-    /usr/bin/touch /etc/squid3/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid3/squid.conf https://raw.githubusercontent.com/hostonnet/squid-proxy-installer/master/squid.conf
-    /sbin/iptables -I INPUT -p tcp --dport 49951 -j ACCEPT
-    /sbin/iptables-save
-    service squid3 restart
-    ln -s /etc/squid3 /etc/squid
-    #update-rc.d squid3 defaults
-    ln -s /etc/squid3 /etc/squid
-elif cat /etc/os-release | grep PRETTY_NAME | grep "jessie"; then
-    # OS = Debian 8
-    /bin/rm -rf /etc/squid
-    /usr/bin/apt update
-    /usr/bin/apt -y install apache2-utils squid3
-    touch /etc/squid3/passwd
-    /bin/rm -f /etc/squid3/squid.conf
-    /usr/bin/touch /etc/squid3/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid3/squid.conf https://raw.githubusercontent.com/hostonnet/squid-proxy-installer/master/squid.conf
-    /sbin/iptables -I INPUT -p tcp --dport 49951 -j ACCEPT
-    /sbin/iptables-save
-    service squid3 restart
-    update-rc.d squid3 defaults
-    ln -s /etc/squid3 /etc/squid
+
+    /usr/bin/htpasswd -b -c /etc/squid/passwd $USERNAME $PASSWORD
+
+    echo ""  | tee -a /etc/sysctl.conf
+    echo "net.ipv4.icmp_echo_ignore_all = 1"  | tee -a /etc/sysctl.conf
+
+    sysctl -p
+
+    echo ""
+    echo "Proxy address: ${green}${USERNAME}:${PASSWORD}@${ip}:${PORT}${reset}"
+
 else
     echo "OS NOT SUPPORTED. Contact admin@hostonnet.com to add support for your OS"
     exit 1;
 fi
 
 
-#/usr/bin/htpasswd -b -c /etc/squid/passwd USERNAME_HERE PASSWORD_HERE
